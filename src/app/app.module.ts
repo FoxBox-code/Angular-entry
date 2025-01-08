512fb0913da6d9016dd2c77c15509980d64ad1fa
Rony Nikolaev <aarontodorov123619@gmail.com>
2025-01-08 23:12:23 +0200

    Starting angular tutuorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProductsModule,
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
